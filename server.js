84ca8a11acb7beba17828bba0ae1cb8778a8c56f
akujobiobinna22 <akujobiobinna22@gmail.com>
2023-08-12 08:50:41 +0100

    login commit

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-"use strict";
 const express = require("express");
 const bodyParser = require("body-parser");
-const sqlite3 = require("sqlite3").verbose(); // Import SQLite
-const path = require("path"); // Import the path module
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path"); // import the path module
+const bcrypt = require("bcrypt");
 
 const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
-
-// Serve static files from the root directory
-app.use(express.static(path.join(__dirname)));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const db = new sqlite3.Database("shipment.db"); // Create or open the SQLite database file
 
@@ -83,6 +82,56 @@ app.put("/updateStatus", (req, res) => {
   );
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
+// User Registration
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  db.run(
+    "INSERT INTO users (username, password) VALUES (?, ?)",
+    [username, hashedPassword],
+    (err) => {
+      if (err) {
+        console.error("Failed to register user:", err);
+        return res.status(500).json({ error: "Failed to register user" });
+      }
+      console.log("User registered successfully:", username);
+      res.json({ message: "User registered successfully" });
+    }
+  );
+});
+
+// User Login
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  db.get(
+    "SELECT * FROM users WHERE username = ?",
+    [username],
+    async (err, user) => {
+      if (err) {
+        return res.status(500).json({ error: "Error while querying database" });
+      }
+
+      if (!user) {
+        return res.status(401).json({ error: "User not found" });
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return res.status(401).json({ error: "Invalid password" });
+      }
+
+      res.json({ message: "Login successful" });
+    }
+  );
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
